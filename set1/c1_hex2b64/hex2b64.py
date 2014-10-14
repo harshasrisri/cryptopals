a62de86cee7ab6e9949403dadc827cd4e7d482bd
Sriharsha <harshasrisri@gmail.com>
2014-10-14 16:39:58 -0700

    hex to b64. although i'd like to do it the C way by playing dirty with individual bits/bytes.

--- set1/c1_hex2b64/hex2b64.py
+++ set1/c1_hex2b64/hex2b64.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 import sys
+import base64
 
 if len(sys.argv) != 2:
 	print "usage: %s <hex_number>" % sys.argv[0]
 	sys.exit()
 
-print "%X" % int(sys.argv[1], 16)
+# hex_num = int(sys.argv[1], 16)
+hex_num = sys.argv[1].decode('hex')
+b64_num = hex_num.encode('base64')
 
+print "Decoded hex: " + hex_num
+print "Encoded B64: " + b64_num
