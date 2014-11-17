b693ed739fed423d71e4fc6c7294fe7758086525
Sriharsha Mucheli <harshasrisri@gmail.com>
2014-11-16 18:51:13 -0700

    modularized single byte xor to use in detect single xor. keyspace expanded from a.zA..Z to include 0..9

--- set1/c3_onekeyxor/onekeyxor.py
+++ set1/c3_onekeyxor/onekeyxor.py
@@ -4,35 +4,37 @@ import string
 
 freq_set = { ' ':13, 'e':12.702, 't':9.056, 'a':8.167, 'o':7.507, 'i':6.966, 'n':6.749, 's':6.327, 'h':6.094, 'r':5.987, 'd':4.253, 'l':4.025, 'u':2.758, 'b':1.492, 'c':2.782, 'f':2.228, 'g':2.015, 'j':0.153, 'k':0.772, 'm':2.406, 'p':1.929, 'q':0.095, 'v':0.978, 'w':2.360, 'x':0.150, 'y':1.974, 'z':0.074 }
 
+keyspace = string.ascii_letters + string.digits
+
 results = dict()
 
 def xorstr (key, msg):
-    temp_str = ""
+    msg = msg.strip();
+    msg = msg.decode('hex')
+    xor_str = ""
     for in_char in msg:
-        temp_str += chr(ord(in_char) ^ ord(key))
-    return temp_str
+        xor_str += chr(ord(in_char) ^ ord(key))
+    return xor_str
 
 def max_rank (msg):
-    for key in string.ascii_letters:
+    for key in keyspace:
         rank = 0
         plaintext = xorstr(key, msg)
         for letter in plaintext:
             if letter in freq_set:
                 rank = rank + freq_set[letter]
         results[key] = rank
-        # print ("%c : %d : %s") % (key, results[key], xorstr(key, msg))
     maxkey = [x for x,y in results.items() if y == max(results.values())]
-    return maxkey
-
+    return (maxkey[0],results[maxkey[0]])
 
 def main ():
     if len(sys.argv) != 2:
         print ("usage: " + sys.argv[0] + " <hex_string>")
         sys.exit()
 
-    msg = sys.argv[1].decode('hex')
-    maxkey=max_rank(msg)
-    print ("%c : %d : %s") % (maxkey[0], results[maxkey[0]], xorstr(maxkey[0], msg))
+    msg = sys.argv[1]
+    maxkey,result=max_rank(msg)
+    print ("%c : %d : %s") % (maxkey, result, xorstr(maxkey, msg))
 
 if __name__ == "__main__":
     main()
--- set1/c4_detect_xor_string/detect_xor_string.py
+++ set1/c4_detect_xor_string/detect_xor_string.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python2
 
 import sys
-sys.path.insert (0,'../c3_1bxorc')
-import 1bxorc
+sys.path.insert (0,'../c3_onekeyxor')
+import onekeyxor
 
 def main () :
+    maxrank = 0
+    for line in open ('4.txt'):
+        key,rank = onekeyxor.max_rank(line)
+        if rank > maxrank:
+            maxrank = rank
+            maxkey = key
+            maxline = line
+    print ("%c : %d : %s") % (maxkey, maxrank, onekeyxor.xorstr(maxkey,maxline))
+    return
 
 if __name__ == "__main__":
     main()
