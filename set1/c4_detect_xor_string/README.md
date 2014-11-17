ebcfb05bf333a6c9b03b09c27a75967dd309f313
Sriharsha Mucheli <harshasrisri@gmail.com>
2014-11-16 18:56:11 -0700

    modified README to include results. parametrized input filename

--- set1/c4_detect_xor_string/README.md
+++ set1/c4_detect_xor_string/README.md
@@ -6,3 +6,9 @@ One of the 60-character strings in [this file](4.txt) has been encrypted by sing
 Find it.
 
 (Your code from #3 should help.)
+
+Results:
+~~~
+./detect_xor_string.py 4.txt 
+5 : 198 : Now that the party is jumping
+~~~
--- set1/c4_detect_xor_string/detect_xor_string.py
+++ set1/c4_detect_xor_string/detect_xor_string.py
@@ -5,8 +5,12 @@ sys.path.insert (0,'../c3_onekeyxor')
 import onekeyxor
 
 def main () :
+    if len(sys.argv) != 2:
+        print ("Usage: " + sys.argv[0] + " <file with ciphertext>")
+        sys.exit()
+
     maxrank = 0
-    for line in open ('4.txt'):
+    for line in open (sys.argv[1]):
         key,rank = onekeyxor.max_rank(line)
         if rank > maxrank:
             maxrank = rank
